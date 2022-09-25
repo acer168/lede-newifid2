1f52f477f25adc57cf9dc65b3706d1344aa7516c
acer168 <114073340+acer168@users.noreply.github.com>
2022-09-25 16:35:53 +0800

    Update newifid2-diy2.sh

--- newifid2-diy2.sh
+++ newifid2-diy2.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+# sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
+# sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 
+# curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
+# curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
+# curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
+# curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yaml > ./package/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+# 修改固件默认名称
+sed -i 's/OpenWrt/NEWIFI/g' package/base-files/files/bin/config_generate
+
+# 修改固件默认时区
+sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
+
+# 管理地址
+sed -i 's/192.168.1.1/192.168.99.2/g' package/base-files/files/bin/config_generate
+
+# 默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+
+# 修改NAS为网络存储
+sed -i 's/"NAS"/"网络存储"/g' `grep "NAS" -rl ./`
