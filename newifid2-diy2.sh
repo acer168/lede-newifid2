cf67ec83b2140864586741f84e0fd66493ecd8f9
acer168 <114073340+acer168@users.noreply.github.com>
2022-10-06 11:51:59 +0800

    Update newifid2-diy2.sh

--- newifid2-diy2.sh
+++ newifid2-diy2.sh
@@ -25,7 +25,7 @@ sed -i 's/OpenWrt/NEWIFI/g' package/base-files/files/bin/config_generate
 sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 
 # 管理地址
-sed -i 's/192.168.1.1/192.168.99.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
 # 默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
